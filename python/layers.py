f7703027d1599b09db1f5b31a555050703c95081
Olexa Bilaniuk <obilaniu@gmail.com>
2017-12-06 07:58:21 -0500

    Add several Matthieu Courbariaux layers.

--- python/layers.py
+++ python/layers.py
@@ -3,7 +3,6 @@
 
 
 #Imports
-from   functional import                    TTQW, Thresh3
 import numpy                                as np
 import os, pdb, sys
 import torch                                as T
@@ -12,6 +11,8 @@ import torch.cuda                           as TC
 import torch.nn                             as TN
 import torch.nn.functional                  as TNF
 import torch.optim                          as TO
+
+from   functional                       import *
 
 
 
@@ -50,6 +51,90 @@ class Conv2dTTQ(TN.Conv2d):
 		                  self.dilation, self.groups)
 
 
+class Conv2dBNN(TN.Conv2d):
+	"""
+	Convolution layer for BinaryNet
+	"""
+	
+	def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+	             padding=0, dilation=1, groups=1, bias=True, H=1.0, W_LR_scale="Glorot"):
+		num_inputs = reduce(lambda a,b:a*b, kernel_size+(in_channels,),  1)
+		num_units  = reduce(lambda a,b:a*b, kernel_size+(out_channels,), 1)
+		if H == "Glorot":
+			self.H          = float(np.sqrt(1.5/(num_inputs + num_units)))
+		else:
+			self.H          = H
+		
+		if W_LR_scale == "Glorot":
+			self.W_LR_scale = float(np.sqrt(1.5/(num_inputs + num_units)))
+		else:
+			self.W_LR_scale = self.H
+		
+		super(Conv2dBNN, self).__init__(in_channels, out_channels, kernel_size,
+		                                stride, padding, dilation, groups, bias)
+		
+		self.reset_parameters()
+	def reset_parameters(self):
+		self.weight.data.uniform_(-self.H, +self.H)
+		if isinstance(self.bias, TN.Parameter):
+			self.bias.data.zero_()
+	def reconstrain(self):
+		self.weight.data.clamp_(-self.H, +self.H)
+	def forward(self, input):
+		Wb = self.weight/self.H
+		Wb = T.clamp((Wb+1.)/2., 0, 1)
+		Wb = T.round(Wb)
+		Wb = (Wb-0.5)*2.0*self.H
+		
+		return TNF.conv2d(input,
+		                  Wb,
+		                  self.bias, self.stride, self.padding,
+		                  self.dilation, self.groups)
+
+
+
+#
+# PyTorch Dense Layers
+#
+
+class LinearBNN(TN.Linear):
+	"""
+	Linear/Dense layer for BinaryNet
+	"""
+	
+	def __init__(self, in_channels, out_channels, bias=True, H=1.0, W_LR_scale="Glorot"):
+		num_inputs = reduce(lambda a,b:a*b, (in_channels,),  1)
+		num_units  = reduce(lambda a,b:a*b, (out_channels,), 1)
+		if H == "Glorot":
+			self.H          = float(np.sqrt(1.5/(num_inputs + num_units)))
+		else:
+			self.H          = H
+		
+		if W_LR_scale == "Glorot":
+			self.W_LR_scale = float(np.sqrt(1.5/(num_inputs + num_units)))
+		else:
+			self.W_LR_scale = self.H
+		
+		super(LinearBNN, self).__init__(in_channels, out_channels, bias)
+		
+		self.reset_parameters()
+	def reset_parameters(self):
+		self.weight.data.uniform_(-self.H, +self.H)
+		if isinstance(self.bias, TN.Parameter):
+			self.bias.data.zero_()
+	def reconstrain(self):
+		self.weight.data.clamp_(-self.H, +self.H)
+	def forward(self, input):
+		Wb = self.weight/self.H
+		Wb = T.clamp((Wb+1.)/2., 0, 1)
+		Wb = T.round(Wb)
+		Wb = (Wb*2 - 1) * self.H
+		
+		return TNF.linear(input, Wb, self.bias)
+
+
+
+
 
 #
 # PyTorch BN/Quantization Layers.
@@ -66,5 +151,19 @@ class BatchNorm2dTz(TN.BatchNorm2d):
 		x = Thresh3.apply(x, self.lo, self.hi)
 		return x
 
+
+
+
+#
+# PyTorch Non-Linearities
+#
+
+class BNNTanh(TN.Module):
+	def __init__(self):
+		super(BNNTanh, self).__init__()
+	
+	def forward(self, x):
+		x = T.clamp((x+1.0)/2.0, 0, 1)
+		return 2.*bnn_round3(x) - 1
 
 
