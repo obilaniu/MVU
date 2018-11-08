383a9b21551ab7dda4e05baf0fb6a04d6ce28761
Olexa Bilaniuk <obilaniu@gmail.com>
2018-11-08 18:52:40 -0500

    Fix vvp.
    Primary problem was a bad assignment.

--- verilog/vvp.v
+++ verilog/vvp.v
@@ -17,24 +17,24 @@ localparam nl = n-nr;
 localparam ar = $clog2(nr);
 localparam al = $clog2(nl);
 
-input  wire             clk;
-input  wire[     1 : 0] mode;
-input  wire[   n-1 : 0] W;
-input  wire[ 2*n-1 : 0] D;
+input  wire                    clk;
+input  wire       [     1 : 0] mode;
+input  wire       [   n-1 : 0] W;
+input  wire       [ 2*n-1 : 0] D;
 
-output wire[ a+2-1 : 0] S;
+output wire signed[ a+2-1 : 0] S;
 
 
 /* Locals */
-wire       [ a+2-1 : 0] Si;
-wire       [ar+2-1 : 0] Sr;
-wire       [al+2-1 : 0] Sl;
+wire signed       [ a+2-1 : 0] Si;
+wire signed       [ar+2-1 : 0] Sr;
+wire signed       [al+2-1 : 0] Sl;
 
 
 /* Modal Multiplier Logic */
-function [1:0] vvp_func(input[1:0] fmode,
-                        input[0:0] fW,
-                        input[1:0] fD);
+function signed[1:0] vvp_func(input[1:0] fmode,
+                              input[0:0] fW,
+                              input[1:0] fD);
 begin
 	if         (fmode == 2'b00) begin /* Weights {+1,-1} */
 		vvp_func = fW ?   -fD :   +fD;
@@ -52,10 +52,7 @@ endfunction
 /* Pipeline Register Insertion */
 generate if(pr & 1) begin:pipe
 	reg [ a+2-1 : 0] R = 0;
-	always @(posedge clk) begin
-		R = Si;
-		//$display("Latching R = %b", R);
-	end
+	always @(posedge clk) R <= Si;
 	assign S = R;
 end else begin:nopipe
 	assign S = Si;
@@ -65,7 +62,7 @@ end endgenerate
 /* Recursive Hardware Logic Generation */
 generate if(n == 1) begin:base
 	assign Si = vvp_func(mode, W, D);
-end else if(n >= 2) begin:calc
+end else if(n >= 2) begin:redux
 	vvp #(nr, pr>>1) r (clk, mode, W[ 0 +: nr], D[  0  +: 2*nr], Sr);
 	vvp #(nl, pr>>1) l (clk, mode, W[nr +: nl], D[2*nr +: 2*nl], Sl);
 	assign Si = {{(a-al){Sl[al+1]}}, Sl} + {{(a-ar){Sr[ar+1]}}, Sr};
@@ -158,9 +155,6 @@ initial begin
 	$display("mode = '%b'", mode);
 	$display("W    = '%b'", W);
 	$display("D    = '%b'", D);
-	$display("master.Sr = '%b'", master.Sr);
-	$display("master.Sl = '%b'", master.Sl);
-	$display("master.S  = '%b'", master.S);
 	$display("S    = '%d'", S);
 	
 	$display("");
@@ -172,22 +166,31 @@ initial begin
 	$display("mode = '%b'", mode);
 	$display("W    = '%b'", W);
 	$display("D    = '%b'", D);
-	$display("C    = '%d'", C);
-	#1;
-	clk = 1; #2; clk = 0; #1; W = {n{1'b0}}; D = {n{2'b0}}; #1;
-	$display("C    = '%d'", C);
-	clk = 1; #2; clk = 0; #1; W = {n{1'b0}}; D = {n{2'b0}}; #1;
-	$display("C    = '%d'", C);
-	clk = 1; #2; clk = 0; #1; W = {n{1'b0}}; D = {n{2'b0}}; #1;
-	$display("C    = '%d'", C);
-	clk = 1; #2; clk = 0; #1; W = {n{1'b0}}; D = {n{2'b0}}; #1;
-	$display("C    = '%d'", C);
-	clk = 1; #2; clk = 0; #1; W = {n{1'b0}}; D = {n{2'b0}}; #1;
-	$display("C    = '%d'", C);
-	clk = 1; #2; clk = 0; #1; W = {n{1'b0}}; D = {n{2'b0}}; #1;
-	$display("C    = '%d'", C);
-	clk = 1; #2; clk = 0; #1; W = {n{1'b0}}; D = {n{2'b0}}; #1;
-	$display("C    = '%d'", C);
+	`define TICK                                                             \
+	        #1;                                                              \
+	        $display("%5t: C    = %3d = [%3d] <-  %3d  <- [%3d] <-  %3d  <- [%3d] <-  %3d  <-  %3d", \
+	                 $time,                                                  \
+	                 C,                                                      \
+	                 clocked.S,                                              \
+	                 clocked.redux.l.S,                                      \
+	                 clocked.redux.l.redux.l.S,                              \
+	                 clocked.redux.l.redux.l.redux.l.S,                      \
+	                 clocked.redux.l.redux.l.redux.l.redux.l.S,              \
+	                 clocked.redux.l.redux.l.redux.l.redux.l.redux.l.S,      \
+	                 clocked.redux.l.redux.l.redux.l.redux.l.redux.l.redux.l.S); \
+	        #1;                                                              \
+	        clk = 1;                                                         \
+	        #3;                                                              \
+	        clk = 0;                                                         \
+	        W = {n{1'b0}}; D = {n{2'b0}};                                    \
+	        #1
+	`TICK;
+	`TICK;
+	`TICK;
+	`TICK;
+	`TICK;
+	`TICK;
+	`TICK;
 end
 
 endmodule
