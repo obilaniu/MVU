a049c5a1a801b5e4201c9e83444158f1afbba5af
Sean Wagner <wagnerse@oc1666018061.ibm.com>
2020-02-20 10:36:26 -0500

    Replaced testBlock macro in quantser_tester with task instead

--- verification/tests/QUANTSER/tb/quantser_tester.sv
+++ verification/tests/QUANTSER/tb/quantser_tester.sv
@@ -49,6 +49,7 @@ quantser #(
 
 
 reg    unsigned[       BDIN-1 : 0]  test_out;   // deserialized output
+test_stats test_stat;
 
 /* Function: predict */
 /* Predicts the expected output */
@@ -59,37 +60,51 @@ function int predict(int x, int bd, int msb);
 endfunction;
 
 
-/* Macro: Quantization test block*/
-`define testBlock(bd, msb, d) \
-    test_out <= 0; \
-    print($sformatf("Test: bdout=%2d,  msbidx=%2d,  din=b%b", bd, msb, d)); \
-    if (msb<bd-1) begin \
-        print("msbidx < bd! Wacky behaviour may ensue!", "WARNING"); \
-    end \
-    if (msb > BDOUTMAX-1 || bd > BDOUTMAX) begin \
-        print($sformatf("msbidx or bd is out of range!"), "WARNING"); \
-    end \
-    din <= d; \
-    bdout <= bd - 1; \
-    msbidx <= msb; \
-    start <= 1; \
-    #(`CLKPERIOD); \
-    start <= 0; \
-    for (int i=0; i < bd; i++) begin \
-        test_out <= test_out << 1; \
-        test_out[0] <= dout; \
-        #(`CLKPERIOD); \
-    end \
-    dout_pred = predict(d, bd, msb); \
-    if (test_out == dout_pred) begin \
-        res_str = "PASS"; \
-        test_stat.pass_cnt+=1; \
-    end \
-    else begin \
-        res_str = "FAIL"; \
-        test_stat.fail_cnt+=1; \
-    end \
-    print($sformatf("  dout=b%32b (%d), Expected: %b [%s]", test_out[bd-1:0], test_out[bd-1:0], dout_pred, res_str));
+
+
+/* Task: quantization test block */
+task testBlock(
+        int bd,
+        int msb,
+        unsigned[   BDIN-1 : 0] d);
+    string testout_str, dout_pred_str, format_str;
+    string res_str;
+    int dout_pred;
+    test_out <= 0;
+    print($sformatf("Test: bdout=%2d,  msbidx=%2d,  din=b%b", bd, msb, d));
+    if (msb<bd-1) begin
+        print("msbidx < bd! Wacky behaviour may ensue!", "WARNING");
+    end
+    if (msb > BDOUTMAX-1 || bd > BDOUTMAX) begin
+        print($sformatf("msbidx or bd is out of range!"), "WARNING");
+    end
+    din <= d;
+    bdout <= bd - 1;
+    msbidx <= msb;
+    start <= 1;
+    #(`CLKPERIOD);
+    start <= 0;
+    for (int i=0; i < bd; i++) begin
+        test_out <= test_out << 1;
+        test_out[0] <= dout;
+        #(`CLKPERIOD);
+    end
+    dout_pred = predict(d, bd, msb);
+    if (test_out == dout_pred) begin
+        res_str = "PASS";
+        test_stat.pass_cnt+=1;
+    end
+    else begin
+        res_str = "FAIL";
+        test_stat.fail_cnt+=1;
+    end
+    //$sformatf(testout_str, "%b", test_out);
+    //$sformatf(dout_pred_str, "%b", dout_pred)
+    format_str = $sformatf("  dout=b%%%0db (%%0d), Expected: b%%%0db (%%0d) [%%s]", bd, bd);
+    print($sformatf(format_str, test_out, test_out, dout_pred, dout_pred, res_str));
+endtask
+
+
 
 
 /* Clock */
@@ -104,10 +119,8 @@ end
 
 /* Run Tests */
 initial begin
-    string msg;
-    string res_str;
-    int dout_pred;
-    automatic test_stats test_stat;
+
+
     print_banner("Testing quantser");
  
     // Initialize signals
@@ -122,17 +135,17 @@ initial begin
     // --------------
 
     // Test 1-bit quantization
-    `testBlock(1, 0, 1)
-    `testBlock(1, 3, 1<<3)
-    `testBlock(1, 31, 1<<31)
+    testBlock(1, 0, 1);
+    testBlock(1, 3, 1<<3);
+    testBlock(1, 31, 1<<31);
  
     // Test 2-bit quantization
-    `testBlock(2, 1, 1<<1)
-    `testBlock(2, 3, 1<<3)
-    `testBlock(2, 31, 1<<31)
+    testBlock(2, 1, 1<<1);
+    testBlock(2, 3, 1<<3);
+    testBlock(2, 31, 1<<31);
 
     // Test 32-bit quantization\
-    `testBlock(32, 31, 5)
+    testBlock(32, 31, 5);
 
 
     // --------------
