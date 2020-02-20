629b4481a4d807d2be90202b9336e086cd6ea640
Sean Wagner <wagnerse@oc1666018061.ibm.com>
2020-02-20 12:22:15 -0500

    Added random tests for quantser testbench

--- verification/tests/QUANTSER/tb/quantser_tester.sv
+++ verification/tests/QUANTSER/tb/quantser_tester.sv
@@ -8,7 +8,7 @@ import utils::*;
 `define CLKPERIOD 10
 
 /* Test paramters */
-`define NUM_TEST_VECS 10
+localparam NUM_TEST_VECS = 100;
 
 
 module quantser_tester();
@@ -48,8 +48,9 @@ quantser #(
 );
 
 
-reg    unsigned[       BDIN-1 : 0]  test_out;   // deserialized output
+
 test_stats test_stat;
+
 
 /* Function: predict */
 /* Predicts the expected output */
@@ -60,16 +61,18 @@ function int predict(int x, int bd, int msb);
 endfunction;
 
 
-
-
 /* Task: quantization test block */
 task testBlock(
         int bd,
         int msb,
         unsigned[   BDIN-1 : 0] d);
-    string testout_str, dout_pred_str, format_str;
-    string res_str;
-    int dout_pred;
+    // Variables
+    string format_str;                          // String format string for output results
+    string res_str;                             // Output PASS or FAIL message
+    logic unsigned[BDOUTMAX-1 : 0] dout_pred;   // predicted output
+    logic unsigned[BDOUTMAX-1 : 0] test_out;    // deserialized output
+
+    // Initialize parameters
     test_out <= 0;
     print($sformatf("Test: bdout=%2d,  msbidx=%2d,  din=b%b", bd, msb, d));
     if (msb<bd-1) begin
@@ -81,6 +84,8 @@ task testBlock(
     din <= d;
     bdout <= bd - 1;
     msbidx <= msb;
+
+    // Start the quantization, and collect the serialized output
     start <= 1;
     #(`CLKPERIOD);
     start <= 0;
@@ -89,6 +94,8 @@ task testBlock(
         test_out[0] <= dout;
         #(`CLKPERIOD);
     end
+
+    // Check the output
     dout_pred = predict(d, bd, msb);
     if (test_out == dout_pred) begin
         res_str = "PASS";
@@ -98,10 +105,12 @@ task testBlock(
         res_str = "FAIL";
         test_stat.fail_cnt+=1;
     end
-    //$sformatf(testout_str, "%b", test_out);
-    //$sformatf(dout_pred_str, "%b", dout_pred)
-    format_str = $sformatf("  dout=b%%%0db (%%0d), Expected: b%%%0db (%%0d) [%%s]", bd, bd);
-    print($sformatf(format_str, test_out, test_out, dout_pred, dout_pred, res_str));
+    
+    // Print results
+    format_str = $sformatf("  dout=b%%%0db (%%0d)", bd);
+    print($sformatf(format_str, test_out, test_out));
+    format_str = $sformatf("  pred=b%%%0db (%%0d) [%%s]\n", bd);
+    print($sformatf(format_str, dout_pred, dout_pred, res_str));
 endtask
 
 
@@ -119,7 +128,7 @@ end
 
 /* Run Tests */
 initial begin
-
+    int test_bd, test_msb, test_din;
 
     print_banner("Testing quantser");
  
@@ -133,24 +142,34 @@ initial begin
     // --------------
     // Boundary tests
     // --------------
-
+    print_banner("Boundary tests: 1-bit");
     // Test 1-bit quantization
     testBlock(1, 0, 1);
     testBlock(1, 3, 1<<3);
     testBlock(1, 31, 1<<31);
  
     // Test 2-bit quantization
+    print_banner("Boundary tests: 2-bit");
     testBlock(2, 1, 1<<1);
     testBlock(2, 3, 1<<3);
     testBlock(2, 31, 1<<31);
 
-    // Test 32-bit quantization\
+    // Test 32-bit quantization
+    print_banner("Boundary tests: 32-bit");
     testBlock(32, 31, 5);
 
 
     // --------------
     // Random tests
     // --------------
+    print_banner("Random tests");
+    for (int i=0; i < NUM_TEST_VECS; i++) begin
+        test_bd = $urandom_range(1, BDOUTMAX);
+        test_msb = $urandom_range(test_bd-1, BDIN-1);
+        test_din = $urandom_range(0, 2**(test_bd)-1);
+        testBlock(test_bd, test_msb, test_din);
+    end
+
 
     print_result(test_stat, VERB_LOW);
     $finish();
