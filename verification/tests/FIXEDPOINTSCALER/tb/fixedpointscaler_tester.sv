c72f65655f2c2481cb1f3b91b850c7de3ce90e2e
wagnersj <wagnerse@ca.ibm.com>
2020-09-09 16:53:49 -0400

    Added random tests for fixed point scaler

--- verification/tests/FIXEDPOINTSCALER/tb/fixedpointscaler_tester.sv
+++ verification/tests/FIXEDPOINTSCALER/tb/fixedpointscaler_tester.sv
@@ -22,13 +22,19 @@ localparam BC     = 27;
 localparam BD     = 27;
 localparam BP     = 45;
 
+// =================================================================================================
+// Typedefs
+typedef logic signed [BA-1 : 0]     a_type;
+typedef logic signed [BA+BB : 0]    m_type;
+typedef logic signed [BP-1 : 0]     p_type;
+
 
 //==================================================================================================
 // Signals
 reg                                clk;            // clock
 reg                                clr;            // clear/reset
 reg    signed      [BA-1 : 0]      a;
-reg                [BB-1 : 0]      b;              // multiplier
+reg    signed      [BB-1 : 0]      b;              // multiplier
 reg    signed      [BC-1 : 0]      c;
 reg    signed      [BD-1 : 0]      d;              // multiplicand
 reg    signed      [BP-1 : 0]      p;              // output product
@@ -75,6 +81,41 @@ end
 
 
 // =================================================================================================
+// Utility functions
+
+task print_signals();
+    print($sformatf("a=%d, b=%d, c=%d, d=%d, p=%d", a, b, c, d, p));
+endtask
+
+
+task random_tests(int count);
+    string res_str;
+    automatic longint expected = 0;
+    automatic test_stats test_stat;
+
+    for (int i = 0; i < count; i++) begin
+        std::randomize(a);
+        std::randomize(b);
+        std::randomize(c);
+        std::randomize(d);
+        expected = p_type'(m_type'((a_type'(a+d))*(int'(b))) + int'(c)); // + longint'(c);
+
+        #(`CLKPERIOD*4);
+
+        if (expected == p) begin
+            res_str = "PASS";
+            test_stat.pass_cnt+=1;
+        end else begin
+            res_str = "FAIL";
+            test_stat.fail_cnt += 1;
+        end
+        print($sformatf("a=%d, b=%d, c=%d, d=%d, p=%d, expected=%d [%s]", a, b, c, d, p, expected, res_str));
+    end
+    print_result(test_stat, VERB_LOW);
+
+endtask
+
+// =================================================================================================
 // Main test thread
 
 initial begin
@@ -91,6 +132,49 @@ initial begin
     // Come out of reset
     clr = 0;
     #(`CLKPERIOD*10);
+
+
+    //-------------------
+    // Simple tests
+
+    print_banner("Simple tests");
+
+    // Set b to 1, expect 0 at output
+    b = 1;
+    #(`CLKPERIOD*4);
+    print_signals();
+
+    // Set a to 1, expect 1 at output
+    a = 1;
+    #(`CLKPERIOD*4);
+    print_signals();
+
+    // Set a to 2, expect 2 at output
+    a = 2;
+    #(`CLKPERIOD*4);
+    print_signals();
+
+    // Set b to 2, expect 4 at output
+    b = 2;
+    #(`CLKPERIOD*4);
+    print_signals();
+
+    // Set c to 1, expect 5 at output
+    c = 1;
+    #(`CLKPERIOD*4);
+    print_signals();
+
+    // Set d to 1, expect 7 at output
+    d = 1;
+    #(`CLKPERIOD*4);
+    print_signals();
+
+
+    //-----------------------------
+    // Random tests
+    print_banner("Random tests");
+    random_tests(20);
+
 
     print_banner($sformatf("Simulation done."));
     $finish();
--- verilog/fixedpointscaler.v
+++ verilog/fixedpointscaler.v
@@ -18,13 +18,13 @@
      BB     = 16,
      BC     = 27,
      BD     = 27,
-     BP     = 45
+     BP     = 48
  )
  (
      input  wire                                clk,            // clock
      input  wire                                clr,            // clear/reset
      input  wire    signed      [BA-1 : 0]      a,
-     input  wire                [BB-1 : 0]      b,              // multiplier
+     input  wire    signed      [BB-1 : 0]      b,              // multiplier
      input  wire    signed      [BC-1 : 0]      c,
      input  wire    signed      [BD-1 : 0]      d,              // multiplicand
      output wire    signed      [BP-1 : 0]      p               // output product
@@ -35,8 +35,8 @@ reg     signed      [BB-1 : 0]  b_q;                   // note: must be signed i
 reg     signed      [BC-1 : 0]  c_q0;
 reg     signed      [BC-1 : 0]  c_q1;
 reg     signed      [BD-1 : 0]  d_q;
-reg     signed      [BD-1 : 0]  preadd_q;
-reg     signed      [BP-1 : 0]  m_q;
+reg     signed      [BA-1 : 0]  preadd_q;
+reg     signed      [BA+BB : 0]  m_q;
 reg     signed      [BP-1 : 0]  p_q;
 
 
@@ -58,7 +58,7 @@ always @(posedge clk) begin
         c_q0 <= c;
         c_q1 <= c_q0;
         d_q <= d;
-        preadd_q <= a_q + d_q;
+        preadd_q <= a + d;
         m_q <= preadd_q * b_q;
         p_q <= m_q + c_q1;
     end
