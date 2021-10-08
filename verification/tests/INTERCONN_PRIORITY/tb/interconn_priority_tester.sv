200b79ed6d0482f4086d0bfc812beb2cee4a92e1
wagnersj <wagnerse@ca.ibm.com>
2021-10-08 12:52:52 -0400

    Added more interconn_priority tests to tb

--- verification/tests/INTERCONN_PRIORITY/tb/interconn_priority_tester.sv
+++ verification/tests/INTERCONN_PRIORITY/tb/interconn_priority_tester.sv
@@ -90,18 +90,25 @@ end
 
 // =================================================================================================
 // Tasks and functions
-task sendData(int from, int to, logic[BADDR-1 : 0] addr, logic[W-1 : 0] word);
+task sendData(int from[], int to[], logic[BADDR-1 : 0] addr, logic[W-1 : 0] word);
 
-    send_to[from] = 1 << to;
-    send_addr[from] = addr;
-    send_word[from] = word;
-    send_en[from] = 1;
+    foreach (from[j]) begin
+        send_to[from[j]] = 0;
+        foreach (to[i]) begin
+            send_to[from[j]] |= 1 << to[i];
+        end
+        send_addr[from[j]] = addr;
+        send_word[from[j]] = word;
+        send_en[from[j]] = 1;
+    end
     #(`CLKPERIOD);
-    send_en[from] = 0;
+    foreach (from[j]) begin
+        send_en[from[j]] = 0;
+    end
 
 endtask
 
-task sendDataAndCheck(int from, int to, logic[BADDR-1 : 0] addr, logic[W-1 : 0] word);
+task sendDataAndCheck(int from[], int to[], logic[BADDR-1 : 0] addr, logic[W-1 : 0] word);
     string res_str;
     logic [W-1 : 0] word_out;
     logic [BADDR-1 : 0] addr_out;
@@ -110,23 +117,30 @@ task sendDataAndCheck(int from, int to, logic[BADDR-1 : 0] addr, logic[W-1 : 0] 
 
     sendData(from, to, addr, word);
 
-    word_out = recv_word[to];
-    addr_out = recv_addr[to];
-    en_out = recv_en[to];
-    from_out = recv_from[to];
-
-    if ((word == word_out[W-1 : 0]) && (addr == addr_out) && en_out && (from_out == (1 << from))) begin
-        res_str = "PASS";
-        test_stat.pass_cnt+=1;
+    // Check the output
+    foreach (from[j]) begin
+        foreach (to[i]) begin
+            word_out = recv_word[to[i]];
+            addr_out = recv_addr[to[i]];
+            en_out = recv_en[to[i]];
+            from_out = recv_from[to[i]];
+            if ((word == word_out[W-1 : 0]) && (addr == addr_out) && en_out && (from_out == (1 << from[j]))) begin
+                res_str = "PASS";
+            end
+            else begin
+                res_str = "FAIL";
+                break;    
+            end
+        end
     end
-    else begin
-        res_str = "FAIL";
-        test_stat.fail_cnt+=1;       
+
+    if (res_str == "PASS") begin
+        test_stat.pass_cnt+=1;  
+    end else begin
+        test_stat.fail_cnt+=1;    
     end
 
-    logger.print($sformatf("from=%d, to=%d, addr=%x, word=%x %s", from, to, addr, word, res_str));
-    //print($sformatf("from_out=%b, en_out=%b, addr_out=%x, word_out=%x", from_out, en_out, addr_out, word_out));
-    //print($sformatf("recv_from=%b, recv_en=%b, recv_addr=%x, recv_word=%x", recv_from, recv_en, recv_addr, recv_word));
+    logger.print($sformatf("from=%d, to=%s, addr=%x, word=%x %s", from, array2str(to), addr, word, res_str));
 
 endtask;
 
@@ -156,6 +170,7 @@ initial begin
     #(`CLKPERIOD*10);
 
     // Test all permutations for 1-to-1
+    logger.print_banner($sformatf("1-1 tests"));
     for (int i = 0; i < N; i++) begin
         if (i % 2) begin
             testword = 'hdeadbeefdeadbeef;
@@ -163,11 +178,35 @@ initial begin
             testword = 'hbeefdeadbeefdead;
         end
         for (int j = 0; j < N; j++) begin
-            sendDataAndCheck(i, j, i+j+1, testword);
+            sendDataAndCheck('{i}, '{j}, i+j+1, testword);
         end
     end
 
+    // Test broadcasting
+    logger.print_banner($sformatf("Broadcast tests: send to 2 neighbouring destinations."));
+    for (int i = 0; i < N; i++) begin
+         if (i % 2) begin
+            testword = 'hdeadbeefdeadbeef;
+        end else begin
+            testword = 'hbeefdeadbeefdead;
+        end
+        for (int j = 0; j < N; j++) begin
+            sendDataAndCheck('{i}, '{j,(j+1)%N}, i+j+1, testword);
+        end
+    end
 
+    // Test broadcasting
+    logger.print_banner($sformatf("Broadcast tests: send to 3 neighbouring destinations."));
+    for (int i = 0; i < N; i++) begin
+         if (i % 2) begin
+            testword = 'hdeadbeefdeadbeef;
+        end else begin
+            testword = 'hbeefdeadbeefdead;
+        end
+        for (int j = 0; j < N; j++) begin
+            sendDataAndCheck('{i}, '{j,(j+1)%N,(j+2)%N}, i+j+1, testword);
+        end
+    end
 
     // End
     print_result(test_stat, VERB_LOW, logger);
